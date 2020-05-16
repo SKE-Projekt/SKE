f1dcc0e42cabaa866e38e1433ae8def84472f295
Kamil Tokarski <skeprojekt@gmail.com>
2020-05-16 09:47:30 +0000

    Lepsze wyswietalnie

--- SKE_COURSES/admin.py
+++ SKE_COURSES/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.db.models import ManyToOneRel, ForeignKey, OneToOneField
+
 from . import models
+
+MySpecialAdmin = lambda model: type('SubClass'+model.__name__, (admin.ModelAdmin,), {
+        'list_display': [x.name for x in model._meta.fields],
+            'list_select_related': [x.name for x in model._meta.fields if isinstance(x, (ManyToOneRel, ForeignKey, OneToOneField,))]
+            })
 
 admin.site.register(models.Course)
 admin.site.register(models.CourseUpload)
 admin.site.register(models.CourseExercise)
-admin.site.register(models.ExerciseSubmission)
-admin.site.register(models.ExerciseExampleShowing)
+admin.site.register(models.ExerciseSubmission, MySpecialAdmin(models.ExerciseSubmission))
+admin.site.register(models.ExerciseExampleShowing, MySpecialAdmin(models.ExerciseExampleShowing))
